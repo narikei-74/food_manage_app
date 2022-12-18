0d8a7dab336f0e518d5e8974cf0ed31ed81e33d6
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2022-12-18 23:07:51 +0900

    食材関連のデザイン作成

---
+++ src/styles/FoodStockEditButtonStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const FoodStockEditButtonStyle = () => {
+  const styles = StyleSheet.create({
+    iconWrapper: {
+      position: "absolute",
+      top: "auto",
+      left: "auto",
+      right: 25,
+      bottom: 40,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    iconContainer: {
+      alignItems: "center",
+      marginTop: 8,
+      marginHorizontal: 10,
+    },
+  });
+
+  return styles;
+};
+
+export default FoodStockEditButtonStyle;
